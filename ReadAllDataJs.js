7fc8b3e6e57e6736b480371675c77d1c91439cea
vinay22-cell <76830596+vinay22-cell@users.noreply.github.com>
2021-01-01 10:34:22 +0530

    Update ReadAllDataJs.js

--- ReadAllDataJs.js
+++ ReadAllDataJs.js
@@ -2,10 +2,10 @@ console.log("Local Storage Database is Ready...")
 var Read_All_Data=document.getElementById("Read_All_Data")
 
 Read_All_Data.onclick=function(){
-    var keys=Object.keys(localStorage)
+    var keys=Object.keys(localStorage)// get the keys from local storage
     console.log("Key"+"    :   "+"Value")
    
-    for(var key of keys){
+    for(var key of keys){ // using the for loop
         console.log(key+":  "+localStorage.getItem(key));
     }
 }
