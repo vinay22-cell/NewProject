f86969aa6ffbc01056a74d611a318782bce86e06
vinay22-cell <76830596+vinay22-cell@users.noreply.github.com>
2021-01-01 10:30:25 +0530

    Update InsertJs.js

--- InsertJs.js
+++ InsertJs.js
@@ -1,19 +1,19 @@
 console.log("Local DataBase Storage is Ready...");
-var take_input_key=document.getElementById("take_input_key")
+var take_input_key=document.getElementById("take_input_key")// take the input from here
 var take_input=document.getElementById("take_input_value")
 var save_value=document.getElementById("save_value")
 
 save_value.onclick=function(){
-    var key=localStorage.getItem(take_input_key.value)
-    if(!(key!=null)){
+    var key=localStorage.getItem(take_input_key.value) // store the key here 
+    if(!(key!=null)){ // checking the condition
         var val=localStorage.getItem(take_input_value.value)
-        if(!(val!=null)){
+        if(!(val!=null)){ // checking the condition
             localStorage.setItem(take_input_key.value,take_input_value.value);
-            console.log("Data Successfully inserted in Your Database...")
+            console.log("Data Successfully inserted in Your Database...")//show message
         }else{
-            console.log("Please Provide a valid not null Value...")
+            console.log("Please Provide a valid not null Value...")// show the error the message
         }
     }else{
-        console.log("PLease Provide the Valid Key...")
+        console.log("PLease Provide the Valid Key...")//show the error message
     }
 }
