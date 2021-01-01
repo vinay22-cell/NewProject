a3d77c5a45f380bb0dde20c9a1969db31f88670c
vinay22-cell <76830596+vinay22-cell@users.noreply.github.com>
2021-01-01 10:37:36 +0530

    Update SearchDataJs.js

--- SearchDataJs.js
+++ SearchDataJs.js
@@ -1,12 +1,12 @@
 console.log("Local Stoage Database is Ready...")
-var read_localstorage=document.getElementById("read_localstorage")
+var read_localstorage=document.getElementById("read_localstorage")// read the input from user
 var search_input=document.getElementById("search_input")
 
 read_localstorage.onclick=function(){
-    var key=localStorage.getItem(search_input.value)
+    var key=localStorage.getItem(search_input.value)// store the key value from here
     if(!(key==null)){
-       console.log("Successfylly fetch from Local DataBase Storage : "+localStorage.getItem(search_input.value))
+       console.log("Successfylly fetch from Local DataBase Storage : "+localStorage.getItem(search_input.value))//perform the operation 
     }else{
-        console.log("Please insert Valid Input...")
+        console.log("Please insert Valid Input...")// show error message
     }
 }
