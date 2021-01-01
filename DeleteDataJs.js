a6b752b188c8b63f5ba71a4cb7c59d0eeb26158e
vinay22-cell <76830596+vinay22-cell@users.noreply.github.com>
2021-01-01 10:19:44 +0530

    Update DeleteDataJs.js

--- DeleteDataJs.js
+++ DeleteDataJs.js
@@ -5,11 +5,11 @@ var delete_input_key=document.getElementById("delete_input_key")
 
 
 delete_data.onclick=function(){
-    var key=localStorage.getItem(delete_input_key.value)
-    if(!(key===null)){
+    var key=localStorage.getItem(delete_input_key.value) //store the input value from the user
+    if(!(key===null)){ // check the condition
     localStorage.removeItem(delete_input_key.value);
-    console.log("Deletion is Succesfull\nPlease Check Your Local DataBase Storage...")
+    console.log("Deletion is Succesfull\nPlease Check Your Local DataBase Storage...") //perform the operation
     }else{
-        console.log("Data is not found in Local DataBase Storage.\n Please Given the Valid Input")
+        console.log("Data is not found in Local DataBase Storage.\n Please Given the Valid Input") //otherwise show error message
     }
 }
